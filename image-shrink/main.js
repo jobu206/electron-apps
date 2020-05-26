2d6fae1fbd25808fbe976711a9e5d512c809fd56
Kevin LeRoy <klleroy@icloud.com>
2020-05-26 06:10:15 -0700

    Implemented imagemin to reduce file size & plugins for file path.

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
 
 process.env.NODE_ENV = 'development';
 
@@ -95,8 +101,29 @@ const menu = [
 ];
 
 ipcMain.on('image:minimize', (e, options) => {
-  console.log(options);
+  options.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQual = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQual, pngQual]
+        })
+      ]
+    });
+    console.log(files);
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.on('window-all-closed', () => {
   if (!isMac) {
