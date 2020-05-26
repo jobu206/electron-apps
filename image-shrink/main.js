d11984441821e06542a0470cf8b85618e48e66ab
Kevin LeRoy <klleroy@icloud.com>
2020-05-26 05:42:04 -0700

    Adding .gitignore to remove node_modules dir. Building redering capabilities.

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 
 process.env.NODE_ENV = 'development';
 
@@ -8,93 +8,104 @@ const isMac = process.platform === 'darwin' ? true : false;
 let mainWindow;
 
 function createMainWindow() {
-	mainWindow = new BrowserWindow({
-		title: 'ImageShrink',
-		width: 500,
-		height: 600,
-		icon: './assets/icons/Icon_256x256.png',
-		resizable: isDev ? true : false,
-		backgroundColor: 'white',
-	});
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: './assets/icons/Icon_256x256.png',
+    resizable: isDev ? true : false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-	mainWindow.loadFile('./app/index.html');
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile('./app/index.html');
 }
 
 function createAboutWindow() {
-	aboutWindow = new BrowserWindow({
-		title: 'ImageShrink',
-		width: 300,
-		height: 300,
-		icon: './assets/icons/Icon_256x256.png',
-		resizable: false,
-		backgroundColor: 'white',
-	});
+  aboutWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 300,
+    height: 300,
+    icon: './assets/icons/Icon_256x256.png',
+    resizable: false,
+    backgroundColor: 'white'
+  });
 
-	aboutWindow.loadFile('./app/about.html');
+  aboutWindow.loadFile('./app/about.html');
 }
 
 app.on('ready', () => {
-	createMainWindow();
+  createMainWindow();
 
-	const mainMenu = Menu.buildFromTemplate(menu);
-	Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-	mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
-	...(isMac
-		? [
-				{
-					label: app.name,
-					submenu: [
-						{
-							label: 'About',
-							click: createAboutWindow,
-						},
-					],
-				},
-		  ]
-		: []),
-	{
-		role: 'fileMenu',
-	},
-	...(!isMac
-		? [
-				{
-					label: 'Help',
-					submenu: [
-						{
-							label: 'About',
-							click: createAboutWindow,
-						},
-					],
-				},
-		  ]
-		: []),
-	...(isDev
-		? [
-				{
-					label: 'Developer',
-					submenu: [
-						{ role: 'reload' },
-						{ role: 'forcereload' },
-						{ type: 'separator' },
-						{ role: 'toggledevtools' },
-					],
-				},
-		  ]
-		: []),
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow
+            }
+          ]
+        }
+      ]
+    : []),
+  {
+    role: 'fileMenu'
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow
+            }
+          ]
+        }
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' }
+          ]
+        }
+      ]
+    : [])
 ];
 
+ipcMain.on('image:minimize', (e, options) => {
+  console.log(options);
+});
+
 app.on('window-all-closed', () => {
-	if (!isMac) {
-		app.quit();
-	}
+  if (!isMac) {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createMainWindow();
-	}
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
 });
